ee5b9faf642d999ee5091a9c2adc16b30a5bef09
p150 <pavanprakash21@gmail.com>
2017-06-17 14:28:05 +0530

    add react and webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!')
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// render on page
+ReactDOM.render(
+  <div>
+    <h1>Hello World! 123</h1>
+  </div>,
+  document.getElementById('app')
+);
