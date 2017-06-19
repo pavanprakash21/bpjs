379b541806b3c002085ee7b66f046cce911a7d4b
p150 <pavanprakash21@gmail.com>
2017-06-19 20:03:39 +0530

    add episode route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home";
 import Series from "./pages/series";
@@ -13,6 +13,7 @@ ReactDOM.render(
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/series/:id" component={Series} />
+        <Route path="/episode/:id" component={Episode} />
         <Route component={Home} />
       </Switch>
     </div>
